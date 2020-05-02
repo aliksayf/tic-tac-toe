868338b435849417da45096505c6e23085f69c54
aliksayf <52054450+aliksayf@users.noreply.github.com>
2020-05-01 21:51:14 -0700

    Updates

---
+++ precache-manifest.c89b4117414372681b4a0ecb6a2d93e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e436aa50b61cf710bcffc465222670f9",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "624549539dc9d875229e",
+    "url": "/tic-tac-toe/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "7ae38cde632e6c0efbe4",
+    "url": "/tic-tac-toe/static/css/main.8c3b3cac.chunk.css"
+  },
+  {
+    "revision": "624549539dc9d875229e",
+    "url": "/tic-tac-toe/static/js/2.949be826.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tic-tac-toe/static/js/2.949be826.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ae38cde632e6c0efbe4",
+    "url": "/tic-tac-toe/static/js/main.2c820210.chunk.js"
+  },
+  {
+    "revision": "f1e36d2c0b0baf77fd90",
+    "url": "/tic-tac-toe/static/js/runtime-main.df38287b.js"
+  }
+]);
